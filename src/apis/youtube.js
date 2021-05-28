69b4eb4e27a42645645e1470f96e2462f5f8ed99
Richie1136 <Richiemajor1@gmail.com>
2021-05-28 16:06:20 -0400

    Deleted the key from the youtubefrontend file and moved to a .env file

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,8 +1,5 @@
 import axios from 'axios'
 
-// const KEY = 'AIzaSyA5GWULMe9i0IOoXanZADXSMLl_Hj7qPCU'
-
-
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
   params: {
