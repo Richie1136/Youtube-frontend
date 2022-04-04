b8f1e4b009748ad18da0f15a3de96e1534c45c65
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-04-04 16:59:40 -0400

    Recieve 403 error code when i hide my api key inside of the .env

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 const KEY = 'AIzaSyA5GWULMe9i0IOoXanZADXSMLl_Hj7qPCU'
 
+// Whenever i hide this API KEY .env i recieve a 403 error code
+
 
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
