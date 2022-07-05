7295e348206ce983906e8377dad3bf47b558b51b
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-07-05 14:31:12 -0400

    Hiding Youtube API key

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,7 +1,12 @@
 import axios from 'axios'
-const KEY = 'AIzaSyA5GWULMe9i0IOoXanZADXSMLl_Hj7qPCU'
+// const KEY = 'AIzaSyA5GWULMe9i0IOoXanZADXSMLl_Hj7qPCU'
 
 // Whenever i hide this API KEY .env i recieve a 403 error code
+
+console.log(process.env.REACT_APP_YOUTUBE_API_KEY)
+let key = process.env.REACT_APP_YOUTUBE_API_KEY
+
+// console.log(KEY)
 
 
 export default axios.create({
@@ -9,6 +14,6 @@ export default axios.create({
   params: {
     part: 'snippet',
     maxResults: 5,
-    key: KEY,
+    key: key,
   }
 });
