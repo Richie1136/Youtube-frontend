3dc63c45aae85aeeadb06dfa1652bdb969bc1b59
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-07-05 14:38:48 -0400

    Storing key inside of env file and created variable to the path

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,13 +1,8 @@
 import axios from 'axios'
-// const KEY = 'AIzaSyA5GWULMe9i0IOoXanZADXSMLl_Hj7qPCU'
 
 // Whenever i hide this API KEY .env i recieve a 403 error code
 
 let key = process.env.REACT_APP_YOUTUBE_API_KEY
-
-let Api = {
-  key: process.env.REACT_APP_YOUTUBE_API_KEY
-}
 
 
 export default axios.create({
@@ -15,6 +10,6 @@ export default axios.create({
   params: {
     part: 'snippet',
     maxResults: 5,
-    key: Api.key,
+    key: key,
   }
 });
