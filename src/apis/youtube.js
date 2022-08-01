1c3b77f0bfcfa756dc109707e0b795aa14f6d699
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-08-01 13:30:15 -0400

    Working to fix 403 error

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const key = process.env.REACT_APP_YOUTUBE_API_KEY
+const APIKEY = process.env.REACT_APP_API_KEY
 
 
 export default axios.create({
@@ -8,6 +8,6 @@ export default axios.create({
   params: {
     part: 'snippet',
     maxResults: 5,
-    key: key,
+    key: APIKEY,
   }
 });
